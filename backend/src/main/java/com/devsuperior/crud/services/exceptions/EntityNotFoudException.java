3ca9db021a1e99f9e0c8023cf63fdb7ce07997d2
Ronaldo Faustino <Ronaldo Faustino>
2022-02-27 13:57:22 -0300

    Crud Client

---
+++ backend/src/main/java/com/devsuperior/crud/services/exceptions/EntityNotFoudException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.crud.services.exceptions;
+
+public class EntityNotFoudException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoudException(String msg) {
+		super(msg);
+	}
+	
+
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1988-07-14T10:00:00Z',0,'33333333332',3525.50,'Francisco de Souza'); 
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1990-09-14T10:00:00Z',3,'45233333332',6525.50,'Maria de Souza');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '2000-05-14T10:00:00Z',0,'65248423332',1525.50,'Emilia de Aguiar');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '2001-12-14T10:00:00Z',1,'45215121532',2525.50,'Suzana Leticia');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1999-11-14T10:00:00Z',4,'54215412151',6525.50,'Lilian de Paiva');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1952-09-14T10:00:00Z',0,'45612654212',1525.50,'Francisco Jose');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1970-10-14T10:00:00Z',5,'33555545555',1225.50,'Lucas Ribeiro');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '1988-12-14T10:00:00Z',1,'33452421515',4512.50,'Athaide Castro');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '2000-01-14T10:00:00Z',2,'12345689115',3525.50,'Mirian Sabatela');
+INSERT INTO tb_client (birth_date, children, cpf, income, name) VALUES(TIMESTAMP WITH TIME ZONE '2003-03-14T10:00:00Z',0,'65455615151',3525.50,'Jiu Joio');
